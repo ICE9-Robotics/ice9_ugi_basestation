9566c12a78b7d17efeef97b1fa57b54298ee3151
Anthony Jiang <tmxkn1@users.noreply.github.com>
2023-11-13 12:16:10 +0000

    Update install_basestation.sh

--- install/install_basestation.sh
+++ install/install_basestation.sh
@@ -19,3 +19,15 @@ source ~/.bashrc
 # Configure mapviz
 cp ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.mvc ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.local.mvc
 sed -i "s|UTGO1PRO.png|${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.png|" ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.local.mvc
+
+# Desktop launcher
+echo "[Desktop Entry]
+Name=Unitree Basestation
+Comment=Launch the base station for Unitree Go1
+Exec=${dir}/install/files/basestation.desktop.sh
+Icon=${dir}/install/files/ice9_logo.png
+Terminal=true
+Type=Application" > Unitree_Basestation.desktop
+chmod +x Unitree_Basestation.desktop
+gio set Unitree_Basestation.desktop "metadata::trusted" true
+mv Unitree_Basestation.desktop ~/Desktop/Unitree_Basestation.desktop
