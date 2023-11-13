a41df777f712e906e5af29e019dca087232b3f67
Anthony Jiang <tmxkn1@users.noreply.github.com>
2023-11-13 11:24:30 +0000

    Update install_basestation.sh

--- install/install_basestation.sh
+++ install/install_basestation.sh
@@ -17,4 +17,5 @@ echo "source ${dir}/basestation_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # Configure mapviz
-sed -i "s|UTGO1PRO.png|${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.png|" ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.mvc
+cp ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.mvc ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.local.mvc
+sed -i "s|UTGO1PRO.png|${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.png|" ${dir}/basestation_ws/src/ice9_unitree_basestation/config/unitree.local.mvc
