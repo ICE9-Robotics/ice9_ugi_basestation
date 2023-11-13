9aeda3fe1378ddc05b418bd0b05c148e90e699c8
Anthony Jiang <tmxkn1@users.noreply.github.com>
2023-11-13 15:10:06 +0000

    Update basestation.desktop.sh

--- install/files/basestation.desktop.sh
+++ install/files/basestation.desktop.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-cd $(dirname "$(readlink -f "$0")")/../..
-dir=$(pwd)
+source ${HOME}/.config/ice9_basestation/setup.bash
+
+# Husarnet
+local_hostname=`husarnet status | grep localhost | awk '{print $3}'`
+if [ ! ${local_hostname} ]; then
+    echo "Can't get Husarnet status. Please check if the software has been properly installed."
+    read -p "Press Enter to continue ..."
+    exit 1
+fi
 
 # ROS setup
 export ROS_IPV6=on
 export ROS_MASTER_URI=http://master:11311
-export ROS_HOSTNAME=basestation_0
-source ${dir}/basestation_ws/devel/setup.bash
+export ROS_HOSTNAME=${local_hostname}
+source ${BASESTATION_DIR}/basestation_ws/devel/setup.bash
 
 # Wait for ros master
+echo "Waiting for ROS master ..."
 while [[ ! $(rostopic list) ]]; do
 	sleep 1
-    echo "Waiting for ros master ..."
+    clear
+    echo "Waiting for ROS master ..."
 done
+echo "Got ROS master!"
 
 # Launch
 roslaunch ice9_unitree_basestation basestation.launch
-read "Press Enter to continue ..."
+read -p "Press Enter to continue ..."
